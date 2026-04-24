d66d002c9186b6d69dec0e967a915f110ce138fd
Henrique <Henriquecoliveira365@gmail.com>
2026-04-24 14:49:59 -0300

    estrutura js separada em routes e controllers e dependencia cors implementada

---
+++ js/routes/login_box.js
@@ -0,0 +1,6 @@
+import server from './js/server.js';
+
+
+APP.get('/', (req, res) => {
+
+})
--- js/server.js
+++ js/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import cors from 'cors';
 
 const APP = express();
 const PORT = 3000;
+
+APP.use(cors());
 
 APP.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
