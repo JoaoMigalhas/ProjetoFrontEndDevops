89d022fc9658b9e35f33c400aa5253086e0588a7
Henrique <Henriquecoliveira365@gmail.com>
2026-05-12 09:06:03 -0300

    o campo dicas de cada usuário agora é atualizado conforme a reinvidicação de dicas na página

--- public/dicas.js
+++ public/dicas.js
@@ -1,13 +1,19 @@
 const buttonDicas = document.getElementById('buttonDicas');
+let contadorDicas = 0;
 
 buttonDicas.addEventListener('click', async () => {
-    try {
-        const response = await fetch('/api/dicas', {
-            method: 'POST',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify(nickname)
-        });
-    } catch(err){
-        console.error(`erro ao pedir dicas: ${err}`);
+    contadorDicas++;
+    if(contadorDicas <= 3) {
+        try {
+            const response = await fetch('/api/dicas', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                body: JSON.stringify({nickname, contadorDicas, filmeSelecionado})
+            });
+        } catch(err){
+            console.error(`erro ao pedir dicas: ${err}`);
+        }
+    } else {
+        alert('Número máximo de dicas para esse filme reivindicado');
     }
 })
--- routes/dicas.js
+++ routes/dicas.js
@@ -1,20 +1,26 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { Router } from 'express';
+import * as userUtils from '../userFunctions.js';
 
 const routes = Router();
 const LOGIN_DB_PATH = "./src/assets/data/users.json";
 
 routes.post('/api/dicas', async (req, res) => {
-    const {nickname} = req.body();
+    const {nickname, contadorDicas, filmeSelecionado} = req.body;
 
-    let users = [];
-    const read = await readFile(LOGIN_DB_PATH, 'utf-8');
-    users = JSON.parse(read);
+    const raw = await readFile(LOGIN_DB_PATH, 'utf-8');
+    const users = JSON.parse(raw);
 
     const usuarioEncontrado = users.find(u => u.nickname === nickname);
 
-    if(usuarioEncontrado) {
+    if(usuarioEncontrado && usuarioEncontrado.dicas > 0) {
+        //diminuindo a quantidade de dicas do usuário
         usuarioEncontrado.dicas -= 1;
+        userUtils.atualizarScore(usuarioEncontrado);
+        await writeFile(LOGIN_DB_PATH, JSON.stringify(users, null, 2), 'utf-8');
+
+        //pegando a dica que o usuário requisitou
+
         res.sendStatus(200);
     } else {
         res.sendStatus(400);
